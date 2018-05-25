57ffd6ad6a0ff4fc8ecc19b378c1732fb7297136
Moss <michael@nizamihosting.co.uk>
2018-05-25 20:52:35 +0100

    Winners popup
    Created winner logic
    Winner popup

--- js/app.js
+++ js/app.js
@@ -105,9 +105,11 @@ function movesCount(){
 
 function winner(){
 
-  if(winningPairs.length === 2){
-    alert("CONGRATULATIONS" + "<br>" + "You are a winner!")
-    console.log("YOU WIN THE GAME")
+  let winningScore = cards.length / 2;
+  console.log(winningScore) 
+  if(winningPairs.length === winningScore){
+    //alert("CONGRATULATIONS" + "<br>" + "//console.log("YOU WIN THE GAME");
+    launchModal();
   }
 
 }
@@ -195,6 +197,36 @@ function buildGame() {
 
 
 buildGame();
+
+
+/* MODEL POP UP JS */
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+function launchModal() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
